384aa820405b99a9c685482a2f96a6799f185f25
Yaakov <48412786+Jocabin@users.noreply.github.com>
2022-06-02 10:42:32 +0200

    fix bug chemin relatif

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  target: 'static',
   css: ["@/assets/css/styles.scss"],
   vite: {
     css: {
