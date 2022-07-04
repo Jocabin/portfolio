e46d859b5aaacd56c5d2db2b6c6fed199ca4d5ba
Yaakov <48412786+Jocabin@users.noreply.github.com>
2022-07-04 19:13:16 +0200

    Connect to firebase project page

--- pages/projets.vue
+++ pages/projets.vue
@@ -3,7 +3,7 @@
     <CardTemplate v-for="project in projectList">
       <CardHeader>
         <h2 class="h1">{{ project.title }}</h2>
-        <p class="texte">{{ project.text }}</p>
+        <p class="texte">{{ project.desc }}</p>
       </CardHeader>
       <CardNav>
         <CardRow>
@@ -32,41 +32,22 @@
 </template>
 
 <script setup>
+import axios from "axios";
 useHead({
   title: "Projets",
 });
 
-const projectList = ref([
-  {
-    title: "Agenda Digital",
-    text: "L’Agenda Digital est une web app from scratch de To-Do list, pour partager les devoirs au sein de ma classe à l’ECV Digital.",
-    demoLink: "https://agendadigital.netlify.app/",
-    codeLink: "https://github.com/Jocabin/agenda_digital",
-  },
-  {
-    title: "Majuscules accentuées",
-    text: "Copier en 1 clic la majuscule accentuée / caractère spécial de son choix.",
-    demoLink:
-      "https://chrome.google.com/webstore/detail/majuscules-et-caract%C3%A8res/jgclphfedglcbhenhaolbhnkbaebeipi?hl=fr&authuser=0",
-    codeLink: "https://github.com/Jocabin/extension_maj_accent",
-  },
-  {
-    title: "TP espace membre",
-    text: "Espace membre crée avec PHP.",
-    demoLink: "",
-    codeLink: "https://github.com/Jocabin/tp_espace_membre",
-  },
-  {
-    title: "Haarnak",
-    text: "Haarnak est une application Android permettant de stocker vos documents à sortir rapidement.",
-    demoLink: "https://jocabin.github.io/haarnak_website/",
-    codeLink: "https://github.com/Jocabin/haarnak_website",
-  },
-  {
-    title: "The Seal Agency",
-    text: "The Seal Agency est le site d'une agence digitale fictive dans le cadre d'un cours",
-    demoLink: "https://gtjesrad.github.io/ECV_DEV_PROJECT/",
-    codeLink: "https://github.com/GTJesraD/ECV_DEV_PROJECT",
-  },
-]);
+const projectList = ref([]);
+
+onMounted(() => {
+  axios
+    .get(
+      "https://portfolio-projects-f87cb-default-rtdb.firebaseio.com/projets.json"
+    )
+    .then((res) => {
+      for (const id in res.data) {
+        projectList.value.unshift(res.data[id]);
+      }
+    });
+});
 </script>
